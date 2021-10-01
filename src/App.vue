b1e2757a944dbdaadfaeffd24614228693b8a140
MascarBone <simone.bonelli@gmail.com>
2021-10-01 17:13:16 +0200

    component Searchbar created, axios get in App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>RICERCA</h1>
+
+    <Searchbar @search="evento"/>
+
+    <Catalog />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+import Searchbar from './components/Searchbar.vue';
+import Catalog from './components/Catalog.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Searchbar,
+    Catalog,
+  },
+
+  methods: {
+    evento(needleQuery) {
+        console.log(needleQuery);
+
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=4b29211095001d1a2e8e6ee5ce71ec04&query=' + needleQuery)
+        .then((response) => {
+            console.log(response);
+        })
+    }
+  },
 }
 </script>
 
 <style lang="scss">
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Searchbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <form>
+            <input type="text" v-model="needle">
+            <button @click.prevent="$emit('search',needle)" type="submit">Cerca</button>
+
+      </form>      
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Searchbar',
+
+    data: function() {
+        return {
+            needle: '',
+
+        }
+    },
+
+    methods: {
+        
+    },
+}
+</script>
+
+<style>
+
+</style>
